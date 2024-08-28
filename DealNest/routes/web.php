8a24b27d6be4b259b425e04f101fdd76631ccae8
Thanhtiu <149652039+Thanhtiu@users.noreply.github.com>
2024-08-28 09:36:49 -0600

    feat - Chia layoute sellers, buyer, source core teamplate seller

--- DealNest/routes/web.php
+++ DealNest/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+
+Route::get('/kenh-nguoi-ban', function(){
+    return view('sellers.index');
+});
